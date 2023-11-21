7095fef005979a7156dc4b18f53dc2bc3239b86c
krish21212 <151596633+krish21212@users.noreply.github.com>
2023-11-20 22:08:27 -0500

    Add files via upload

---
+++ j.js
@@ -0,0 +1,28 @@
+var tablinks=document.getElementsByClassName("tab-links")
+var tabcontents=document.getElementsByClassName("tab-contents")
+function opentab(tabname)
+{
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+function hello() {
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+if(name=="" || email=="")
+{
+    window.alert("Fill the all fields");
+}
+else{
+window.alert("submitted successfuly")
+}
+
+
+}
